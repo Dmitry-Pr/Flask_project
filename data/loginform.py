8403bec5a85064e9012007b4dd869d26678b68fd
Dmitry-Pr <61019019+Dmitry-Pr@users.noreply.github.com>
2021-04-27 17:33:37 +0300

    Update loginform.py

--- data/loginform.py
+++ data/loginform.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
+    '''Форма входа в аккаунт'''
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
